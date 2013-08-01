4b586ab539ccae264ff11b20bdf0f08c4a3d0e42
Fumihiro Kato <fumi@fumi.me>
2013-08-01 10:53:21 +0900

    Added ja settings into HomepageExtractorConfig.scala

--- core/src/main/scala/org/dbpedia/extraction/config/mappings/HomepageExtractorConfig.scala
+++ core/src/main/scala/org/dbpedia/extraction/config/mappings/HomepageExtractorConfig.scala
@@ -21,6 +21,7 @@ object HomepageExtractorConfig
         "fr" -> Set("website", "homepage", "web", "site", "siteweb", "site web"),/*cleanup*/
         "ga" -> Set("suíomh"),
         "it" -> Set("homepage", "sito", "sito web"),
+        "ja" -> Set("homepage", "website", "ホームページ", "ウェブサイト", "Webサイト", "Ｗｅｂサイト"),
         "nl" -> Set("website", "homepage", "hoofdpagina", "webpagina", "web", "site"),
         "pl" -> Set("web", "strona"),
         "pt" -> Set("website", "homepage", "web", "site", "siteweb", "site web", "página", "sitio", "pagina"),/*cleanup*/
@@ -46,6 +47,7 @@ object HomepageExtractorConfig
         "fr" -> "(?:Lien externe|Liens externes|Liens et documents externes)",
         "ga" -> "(?:Naisc sheachtracha|Nasc sheachtrach)",
         "it" -> "Collegamenti esterni",
+        "ja" -> "外部リンク",
         "nl" -> "(?:Externe links|Externe link)",
         "pl" -> "(?:Linki zewnętrzne|Link zewnętrzny)",
         "pt" -> "(?:Ligações externas|Ligação externa|Links externos|Link externo)",
@@ -69,6 +71,7 @@ object HomepageExtractorConfig
         "fr" -> "officiel",
         "ga" -> "oifigiúil",
         "it" -> "ufficiale",
+        "ja" -> "公式",
         "nl" -> "(?:officieel|officiële)",
         "pl" -> "oficjalna",
         "pt" -> "oficial",
