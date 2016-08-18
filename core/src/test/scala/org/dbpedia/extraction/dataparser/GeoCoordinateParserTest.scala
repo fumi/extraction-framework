7518f631e727db66c14cb90900d808007fa83181
Fumihiro Kato <fumi@fumi.me>
2016-08-18 16:59:14 +0900

    Supported Japanese coordinate templates

--- core/src/test/scala/org/dbpedia/extraction/dataparser/GeoCoordinateParserTest.scala
+++ core/src/test/scala/org/dbpedia/extraction/dataparser/GeoCoordinateParserTest.scala
@@ -18,7 +18,7 @@ class GeoCoordinateParserTest extends FlatSpec with Matchers
     {
         parse("fr", "51º12'00\"N 03º13'00\"E") should equal (Some(51.2,3.216666666666667))
     }
-    "GeoCoordinateParser({{coord|51.2|N|31.2|E}}" should "return (51.2,31.2)) for French" in
+    "GeoCoordinateParser({{coord|51.2|N|31.2|E}})" should "return (51.2,31.2)) for French" in
     {
         parse("fr", "{{coord|51.2|N|31.2|E}}") should equal (Some(51.2,31.2))
     }
@@ -26,7 +26,26 @@ class GeoCoordinateParserTest extends FlatSpec with Matchers
     {
         parse("fr", "{{coord|51/12/N|03/13/E}}") should equal (Some(51.2,3.216666666666667))
     }
-    
+    "GeoCoordinateParser({{ウィキ座標|35|40|12|N|139|46|12|E|scale:10000|銀座NTT}})" should "return (35.67,139.77)) for Japanese" in
+    {
+        parse("ja", "{{ウィキ座標|35|40|12|N|139|46|12|E|scale:10000|銀座NTT}}") should equal (Some(35.67,139.77))
+    }
+    "GeoCoordinateParser({{ウィキ座標度分秒|35|40|12|N|139|46|12|E|}})" should "return (35.67,139.77)) for Japanese" in
+    {
+        parse("ja", "{{ウィキ座標度分秒|35|40|12|N|139|46|12|E|}}") should equal (Some(35.67,139.77))
+    }
+    "GeoCoordinateParser({{ウィキ座標度分|35|40|12|N|139|46|12|E||改行=yes}})" should "return (35.67,139.77)) for Japanese" in
+    {
+        parse("ja", "{{ウィキ座標度分|35|40|12|N|139|46|12|E||改行=yes}}") should equal (Some(35.67,139.77))
+    }
+    "GeoCoordinateParser({{ウィキ座標度|35|40|12|N|139|46|12|E|}})" should "return (35.67,139.77)) for Japanese" in
+    {
+        parse("ja", "{{ウィキ座標度|35|40|12|N|139|46|12|E|}}") should equal (Some(35.67,139.77))
+    }
+    "GeoCoordinateParser({{ウィキ座標2段度分秒|35|40|12|N|139|46|12|E|}})" should "return (35.67,139.77)) for Japanese" in
+    {
+        parse("ja", "{{ウィキ座標2段度分秒|35|40|12|N|139|46|12|E|}}") should equal (Some(35.67,139.77))
+    }
 
     private val wikiParser = WikiParser.getInstance()
 
